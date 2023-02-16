5a80cfc7572f0beb1422a139ba1729b802c07119
AdamsSandile <sandileadams.45@gmailcom>
2023-02-16 09:27:29 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,18 +3,15 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always 1
+ * Return: Always 1 (Error status)
  */
 int main(void)
 {
-    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len;
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = 59; /* Length of the message excluding null terminator */
 
-    len = sizeof(msg) - 1;
-    if (write(STDERR_FILENO, msg, len) != len)
-    {
-        /* Error occurred, return non-zero status */
-        return (1);
-    }
-    return (1);
+	if (write(STDERR_FILENO, msg, len) != len)
+		return (1);
+
+	return (1);
 }
