d7cd2d4e26e2d1772972101afca335cd7cb9dde4
AdamsSandile <sandileadams.45@gmailcom>
2023-02-16 09:20:59 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,20 @@
 #include <unistd.h>
 
-int main(void) {
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
     char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, msg, sizeof(msg) - 1);
-    return 1;
+    ssize_t len;
+
+    len = sizeof(msg) - 1;
+    if (write(STDERR_FILENO, msg, len) != len)
+    {
+        /* Error occurred, return non-zero status */
+        return (1);
+    }
+    return (1);
 }
