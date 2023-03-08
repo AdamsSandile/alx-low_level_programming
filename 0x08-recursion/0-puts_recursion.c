59f3616f5135fde0a70c235e3ab4af408312e089
AdamsSandile <sandileadams.45@gmailcom>
2023-03-08 03:34:44 -0800

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,16 +6,15 @@
  * Return: No.
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	 if (*s == '\0')
+	if (*s != '\0')
 	{
-		   _putchar(*s); 
-                   _puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
 	_putchar('\n');
 	}
-}
-	
+}	
