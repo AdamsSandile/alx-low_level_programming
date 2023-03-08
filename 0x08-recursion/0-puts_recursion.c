2433363384e36f8b93301cef114a2b6df4003c60
AdamsSandile <sandileadams.45@gmailcom>
2023-03-08 03:48:27 -0800

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,8 +2,9 @@
 
 /**
  * _puts_recursion - a function that prints a string, followed by a new line.
- * @s: An input string to printing
- * Return: Nothing
+ * @s: A pointer to the input string to print.
+ *
+ * Return: Nothing.
  */
 void _puts_recursion(char *s)
 {
@@ -13,5 +14,8 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 	else
+	{
 		_putchar('\n');
-}	
+	}
+}
+	
