a054ad69b10ced95088f3776a56636dca0d9feb2
AdamsSandile <sandileadams.45@gmail.com>
2023-04-05 04:50:27 -0700

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,38 +1,71 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
+ * get_safe_list - creates a safe list for printing and checking
+ * @oldList: list of nodes
+ * @size: size of the current list
+ * @node: current node that is being checked in printing
  *
- * Return: number of elements in the freed list
+ * Description: creates a new list that can be checked against for
+ *              possible looped lists.
+ *
+ * Return: const list that can be used to check.
+ */
+listint_t **get_safe_list(listint_t **oldList, int size, listint_t *node)
+{
+	listint_t **safeList;
+	int i;
+
+	safeList = malloc(size * sizeof(listint_t *));
+	if (safeList == NULL)
+	{
+		free(oldList);
+		exit(98);
+	}
+
+	for (i = 0; i < size - 1; i++)
+		safeList[i] = oldList[i];
+
+	safeList[i] = node;
+	free(oldList);
+	return (safeList);
+}
+
+/**
+ * free_listint_safe - safely frees a listint_t list
+ * @h: pointer to the head node of the list
+ *
+ * Description: lists may be unsafe and loopy, this function frees lists.
+ *
+ * Return: number of nodes freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
-
-	if (!h || !*h)
-		return (0);
+	listint_t *next_node;
+	listint_t **safeList = NULL;
+	int i = 0, j = 0;
 
 	while (*h)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
+		while (j < i)
 		{
-			temp = (*h)->next;
-			*h = temp;
-			len++;
+			if (*h == safeList[j])
+			{
+				*h = NULL;
+				free(safeList);
+				return (i);
+			}
+			j++;
 		}
-		else
-		{
-			*h = NULL;
-			len++;
-			break;
-		}
+
+		i++, j = 0;
+		safeList = get_safe_list(safeList, i, *h);
+		next_node = (*h)->next;
+		free(*h);
+		*h = next_node;
 	}
 
-	*h = NULL;
-
-	return (len);
+	free(safeList);
+	return (i);
 }
+
