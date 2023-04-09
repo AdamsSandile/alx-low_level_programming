c7ba5b40fbafc3e12442d5edeb902b9696f018ff
AdamsSandile <sandileadams.45@gmail.com>
2023-04-09 06:31:46 -0700

    ready set go

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,20 +1,13 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness.
- *
- * Return: 0.
- *         
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-    int me= 1;
-    char *ptr = (char *)&me;
+	unsigned int y = 1;
+	char *t = (char *) &y;
 
-    /* If the first byte is 1, the machine is little endian */
-    if (*ptr == 1)
-        return 1;
-    else
-        return 0;
+	return (*t);
 }
-
