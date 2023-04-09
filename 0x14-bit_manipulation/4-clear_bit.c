4842acb5eb6b9c539676581aa4195eaf322d35c2
AdamsSandile <sandileadams.45@gmail.com>
2023-04-09 05:47:47 -0700

    loose lips

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,11 +10,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(*n) * 8)
-        return 0;
-
-    *n &= ~(1UL << index);
-
-    return 1;
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
 }
-
